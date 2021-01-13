d89079f6fff412f11f3e05f75e3c4f2cee02445c
Utkarsh <bajpaiutk@gmail.com>
2021-01-13 11:28:16 +0530

    Displaying database items

--- hielo/views.py
+++ hielo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Todolist
+
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	link_items = Todolist.objects.order_by('id')#Id for seuqential order
+	context = {'link_items' : link_items}
+	return render(request, 'index.html', context)
