02a0fcc5a029cbb2743e3a8fda51574ca3ed474e
Utkarsh <bajpaiutk@gmail.com>
2021-01-14 00:30:02 +0530

    Committing the form thing

--- hielo/views.py
+++ hielo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Todolist
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
@@ -8,3 +11,12 @@ def index(request):
 	link_items = Todolist.objects.order_by('id')#Id for seuqential order
 	context = {'link_items' : link_items}
 	return render(request, 'index.html', context)
+
+@require_POST
+def addTodoItem(request):
+	form = TodoListForm(request.POST)
+	
+	if form.is_valid(): 
+		newTodo = Todolist(text = request.POST['text'])
+		newTodo.save()
+		return redirect('index')
